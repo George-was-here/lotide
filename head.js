511a900769a4559f9a6ba8e618c52ed8a24997a9
George-was-here <george.burt@gmail.com>
2022-04-04 23:47:44 +0000

    Added head aka first item of array returner

--- head.js
+++ head.js
@@ -1,5 +1,12 @@
+
 const assertEqual = function(actual, expected) {
   (actual === expected) ? console.log(`🏅🏅🏅Assertion Passed: ${actual} === ${expected}`) : console.log(`😵‍💫😵‍💫😵‍💫Assertion Failed:  ${actual} !== ${expected}`);
 };
+const head = function (assertEqual) {
+  let firstElement = assertEqual[0];
+  return firstElement;
+  console.log(head);
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-assertEqual("Lighthouse Labs", "Bootcamp");
