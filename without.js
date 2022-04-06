bc37f8f3a277fee4d054fdac8ce89e66b04d05f3
George-was-here <george.burt@gmail.com>
2022-04-06 19:48:47 +0000

    without.js completed and linty

--- without.js
+++ without.js
@@ -21,51 +21,14 @@ let eqArrays = function(array1, array2) {
   return true;
 };
 
-//WITHOUT IS BELOW
-
 const without = (sourceArray, itemsToRemove) => {
-  const removedArray = [...sourceArray];
-  let removalElementIndex;
-  for (let i = 0; i < itemsToRemove.length; i++) {
-    removalElementIndex = removedArray.indexOf(itemsToRemove[i]);
-    if (removalElementIndex > -1) {
-      removedArray.splice(removalElementIndex, 1); 
+  const removedArray = sourceArray.filter(element => {
+    const shouldRemove = itemsToRemove.includes(element);
+    if (shouldRemove) {
+      return false;
+    } else {
+      return true;
     }
-  }
+  });
   return removedArray;
 };
-console.log(without(["1", "2", "3"], [1, 2, "3"]));
-
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
-
-/*
-    
-
-if (array1[i] !== array2[i]) {
-
-
-}
-  
-  if (array1.length !== array2.length) return true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-
-const without = (sourceArray, itemsToRemove) => {
-
-  deletionFilter = function(itemsToRemove) {
-  for (let i = 0; i < sourceArray.length; i++)
-  return itemsToRemove;
-  console.log(itemsToRemove);
-}
-const returnedArray = sourceArray.filter(deletionFilter);
-console.log(returnedArray);
-};
-
-without([1, 2, 3], [1]) 
-without(["1", "2", "3"], [1, 2, "3"])
-*/
-
