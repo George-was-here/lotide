d97926ebb48df92b009956458479919553799192
George-was-here <george.burt@gmail.com>
2022-04-06 04:01:46 +0000

    assertArraysEqual function created and linted

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    console.log(`🏅🏅🏅Assertion Passed: ${array1} === ${array2}`);
+    return true;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      (`😵‍💫😵‍💫😵‍💫Assertion Failed:  ${array1} !== ${array2}`);
+      return false;
+    }
+  }
+};
