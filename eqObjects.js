cdbae3f01648bd97d86ba62af5e574818bd00c9a
George-was-here <george.burt@gmail.com>
2022-04-07 04:51:17 +0000

    Added finalized eqArrays function to lotide, twas linty upon yonder shores.

--- eqObjects.js
+++ eqObjects.js
@@ -3,8 +3,8 @@ const assertEqual = function(actual, expected) {
 };
 
 let eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) 
-  return false;
+  if (array1.length !== array2.length)
+    return false;
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -24,18 +24,26 @@ const eqObjects = function(object1, object2) {
     if (!object2[key])  {
       return false;
     }
-    if (object1[key] !== object2[key]) {
+
+    const isValue1Array = Array.isArray(object1[key]);
+    const isValue2Array = Array.isArray(object2[key]);
+
+    if (isValue1Array !== isValue2Array) {
       return false;
     }
+
+    if (isValue1Array && isValue2Array) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+
     
   }
 
   return true;
 };
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(assertEqual(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertEqual(cd, cd2));
