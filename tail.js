2acf485dccf40a852a3aab573e7a887fef1c21b5
George-was-here <george.burt@gmail.com>
2022-04-12 04:29:51 +0000

    Fixed error in Tail, Tail Test implemented with Mocha

--- tail.js
+++ tail.js
@@ -1,13 +1,13 @@
 const assertEqual = require('./assertEqual');
 
-const tail = function(array) {
-  const tailArray = [];
-  tailArray.push(array.slice(1));
-  return tailArray;
+const tail = (arr) => {
+  return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
 
 module.exports = tail;
+
+// console.log(tail(["INI", "MINI", "MINNIE", "MOE"]));x
